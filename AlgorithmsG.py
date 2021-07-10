0bb3b287c8473481386981b0aca842ac105fe789
QuantumStatic <utkarshworkemail@gmail.com>
2021-07-10 13:00:24 +0800

    Added new functions

--- AlgorithmsG.py
+++ AlgorithmsG.py
@@ -1,8 +1,10 @@
-from math import sqrt, ceil, floor, log
-from myFunctionsG import execute_this, nearMatching
+from math import sqrt, gcd, floor, ceil
+from myFunctions import execute_this, nearMatching, compare_these
 from random import randint as rint
 from statistics import mean
 from functools import lru_cache
+
+
 
 
 def polynomial_eval(polynomial, value):
@@ -41,9 +43,19 @@ def horner_recur(polynomial, value):
 
 
 def horner_alt(polynomial, value):
+
+    def power(a,b):
+        final =0
+        while (b):
+            if b & 1:
+                final *= a
+            a *= a
+            b //= 2
+        return final
+
     evaluation = int()
-    for coefficient, power in enumerate(polynomial):
-        evaluation += coefficient * pow(value, power)
+    for coefficient, powe in enumerate(polynomial):
+        evaluation += coefficient * power(value, powe)
     return (evaluation)
 
 
@@ -57,7 +69,46 @@ def fibCache(n):
         return n
     return fibCache(n - 1) + fibCache(n - 2)
 
+
+def dynamicFib(n):
+    register = dict()
+    register[0], register[1] = 0, 1
+    
+    def fib(n):
+        try:
+            return register[n]
+        except:
+            register[n] = fib(n-1) + fib (n-2)
+            return register[n]
+    
+    return fib(n)
+
+
+def fibMatrixExp(n):
+    # any fibonacci number at nth postition can be written as as (n-1)th power of the matrix [[1,1][1,0]] mulitplied by [fibonaaci(1), fibonacci(0)]
+    result = tuple([[1,0],[0,1]])
+    OG = tuple([[1,1],[1,0]])
+    n -= 1
+
+    def matrixMultiply(a,b):
+        fin = tuple([[0,0],[0,0]])
+        fin[0][0] = (a[0][0]*b[0][0] + a[0][1]*b[1][0]) #% 10
+        fin[0][1] = (a[0][0]*b[0][1] + a[0][1]*b[1][1]) #% 10
+        fin[1][0] = (a[1][0]*b[0][0] + a[1][1]*b[1][0]) #% 10
+        fin[1][1] = (a[1][0]*b[0][1] + a[1][1]*b[1][1]) #% 10
+        return fin
+    
+    while n:
+        if n & 1: 
+            result = matrixMultiply(OG,result) 
+        OG = matrixMultiply(OG,OG) 
+        n //= 2
+    
+    return matrixMultiply(result,tuple([[1,0],[0,1]]))[0][0]
+
+
 def find_max_sub_array_recur(arrey):
+
     # this is found by running brute force against the recursive implementation. Depends on hardware specifications.
     recur_better_than_brute_point = 8
 
@@ -83,7 +134,7 @@ def find_max_sub_array_recur(arrey):
             currSum += arr[x]
             if currSum > rightSum:
                 rightSum = currSum
-                maxright = x
+                maxRight = x
 
         return (maxLeft, maxRight, leftSum + rightSum)
 
@@ -180,3 +231,266 @@ def IorderSelection(List, order):
         return
     
     return Select(List, 0, len(List))
+
+
+def longestCommonSubsequence(seq1:str, seq2:str, only_length = False):
+    seq1_len, seq2_len = len(seq1), len(seq2)
+    matching_sequence_table = list() 
+    
+    if not only_length:
+        for _ in range(seq1_len + 1):
+            matching_sequence_table.append([0] * (seq2_len + 1))
+        
+        for x in range(1, seq1_len + 1):
+            for y in range(1, seq2_len + 1):
+                if seq1[x-1] == seq2[y-1]:
+                    matching_sequence_table[x][y] = 1 + matching_sequence_table[x-1][y-1]
+                else:
+                    matching_sequence_table[x][y] = max(matching_sequence_table[x-1][y], matching_sequence_table[x][y-1])
+        
+        LCS_len = matching_sequence_table[seq1_len][seq2_len]
+        longest_common_subsequence = [0] * LCS_len
+        x, y = seq1_len, seq2_len
+        while matching_sequence_table[x][y] != 0:
+            if matching_sequence_table[x][y - 1] != matching_sequence_table[x][y]:
+                longest_common_subsequence[LCS_len - 1] = seq2[y - 1]
+                x -= 1
+                LCS_len -= 1
+            y -= 1
+
+        return ''.join(longest_common_subsequence)
+    
+    else:
+        for _ in range(2):
+            matching_sequence_table.append([0] * (seq2_len + 1))
+
+        for _ in range(seq1_len):
+            for y in range(1, seq2_len + 1):
+                if seq1[0] == seq2[y-1]:
+                    matching_sequence_table[1][y] = 1 + matching_sequence_table[0][y-1]
+                else:
+                    matching_sequence_table[1][y] = max(matching_sequence_table[0][y], matching_sequence_table[1][y-1])
+            seq1 = seq1[1:]
+            matching_sequence_table[0] = matching_sequence_table[1]
+
+        return matching_sequence_table[1][seq2_len]
+
+
+def binaryExponentiation(a: int, b: int, modulo: int = None):
+    
+    result = 1
+    if modulo is None:
+        modulo = a + 1
+    
+    while b:
+        if b & 1:
+            result *= a
+            result %= modulo
+        a *= a
+        a %= modulo
+        b //= 2
+    
+    return result
+
+
+def jugProblem(jug1:int, jug2:int, toMeasure:int, justSteps = False):
+    smallerJugSize, biggerJugSize, jugToFill =  jug1, jug2, toMeasure
+    
+    if jugToFill % gcd(smallerJugSize, biggerJugSize) is not 0:
+        print("No Solution")
+        return
+    
+    if smallerJugSize > biggerJugSize:
+        smallerJugSize, biggerJugSize = biggerJugSize, smallerJugSize
+
+    steps, steps_taken = int(), list()
+    smallJug = bigJug =  int()
+    while bigJug != jugToFill and smallJug != jugToFill:
+        if bigJug is 0:
+            bigJug = biggerJugSize
+        elif smallJug == smallerJugSize:
+            smallJug = 0
+        else:
+            spaceInSmallerJug = smallerJugSize - smallJug
+            smallJug, bigJug = smallJug + (spaceInSmallerJug if bigJug >= spaceInSmallerJug else bigJug), bigJug - (spaceInSmallerJug if bigJug >= spaceInSmallerJug else bigJug)
+        steps_taken.append((smallJug, bigJug))
+        steps += 1
+
+    if justSteps:
+        return steps
+    else:
+        return steps_taken
+
+
+def constructionProblem(toConstruct:int, pieces:tuple, total_ways:bool = False, minimum_pieces:bool = True, sequence:bool = False) -> int:
+    constructionTable = list()
+    nOfPieces = len(pieces)
+    
+    if total_ways:
+        for _ in range(2):
+            constructionTable.append(list())
+        
+        if pieces[0] is 0:
+            constructionTable[0].append(1)
+            constructionTable[0].extend([0 for _ in range(toConstruct)])
+        else:
+            for x in range(toConstruct+1):
+                constructionTable[0].append(1 if x % pieces[0] == 0 else 0)
+        constructionTable[1].extend([0 for _ in range(toConstruct+1)])
+        
+        pieces = pieces[1:]
+        
+        for _ in range(nOfPieces-1):
+            for y in range(toConstruct+1):
+                ways = constructionTable[0][y]
+                if pieces[0] <= y:
+                    ways += constructionTable[1][y - pieces[0]]
+                constructionTable[1][y] = ways
+            pieces = pieces[1:]
+            constructionTable[0] = constructionTable[1]
+
+        return constructionTable[1][toConstruct]
+    
+    else:
+        if sequence:
+            for _ in range(nOfPieces):
+                constructionTable.append(list())
+            
+            if pieces[0] is 0:
+                constructionTable[0].append(1)
+                constructionTable[0].extend([0 for _ in range(toConstruct)])
+            else:
+                for x in range(toConstruct+1):
+                    constructionTable[0].append(x // pieces[0] if x % pieces[0] == 0 else 0)
+
+            for x in range(1, nOfPieces):
+                for y in range(toConstruct+1):
+                    ways = constructionTable[x-1][y]
+                    if pieces[x] <= y:
+                        ways = min(1 + constructionTable[x][y - pieces[x]], ways)
+                    constructionTable[x].append(ways)
+            
+            x = nOfPieces - 1 
+            y = toConstruct
+            sequence = list()
+            while x is not 0:
+                if constructionTable[x][y] != constructionTable[x-1][y]:
+                    sequence.append(pieces[x])
+                    y -= pieces[x]
+                else:
+                    x -= 1
+
+            return tuple(sequence)
+
+        else:
+            for _ in range(2):
+                constructionTable.append(list())
+        
+            if pieces[0] is 0:
+                constructionTable[0].append(1)
+                constructionTable[0].extend([0 for _ in range(toConstruct)])
+            else:
+                for x in range(toConstruct+1):
+                    constructionTable[0].append(1 if x % pieces[0] == 0 else 0)
+            constructionTable[1].extend([0 for _ in range(toConstruct+1)])
+            
+            pieces = pieces[1:]
+            
+            for _ in range(nOfPieces-1):
+                for y in range(toConstruct+1):
+                    ways = constructionTable[0][y]
+                    if pieces[0] <= y:
+                        ways = min(constructionTable[1][y - pieces[0]], ways)
+                    constructionTable[1][y] = ways
+                pieces = pieces[1:]
+                constructionTable[0] = constructionTable[1]
+
+            return constructionTable[1][toConstruct]
+
+def primeGenerator(limit):
+    
+    isprime, smallestPrimeFactor, primes = [False] * 2 + [True] * (limit - 2), [int()] * limit, list()
+    primes_found = int()
+    for i in range(2, limit): 
+        if isprime[i]: 
+            primes.append(i) 
+            smallestPrimeFactor[i] = i 
+            primes_found += 1
+        j = int()
+        while j < primes_found and i * primes[j] < limit and primes[j] <= smallestPrimeFactor[i]:
+            isprime[i * primes[j]] = False
+            smallestPrimeFactor[i * primes[j]] = primes[j]
+            j += 1
+    return primes
+
+def prime_checker(suspected_prime):
+    # Checking primes since '99. supports lists and individual numbers as well
+    if isinstance(suspected_prime, list):
+        dummy = list()
+        for prime_candidate in suspected_prime:
+            dummy.append(prime_checker(prime_candidate))
+        return dummy
+    else:
+        suspected_prime = abs(suspected_prime)
+        if suspected_prime == 1 or suspected_prime == 2 or suspected_prime == 3:
+            return (False if suspected_prime == 1 else True)
+        if suspected_prime % 2 == 0 or suspected_prime % 3 == 0:
+            return False
+        end_point, prime_factor = ceil(suspected_prime**0.5), 5
+        while True:
+            if end_point < prime_factor:
+                return True
+            if suspected_prime % prime_factor == 0 or suspected_prime & (prime_factor + 2) == 0:
+                return False
+            prime_factor += 6
+
+def sieveErato(limit):
+    # Sieve of Eratothenes. Looks up prime numbers upto almost 8 million in a second.
+    if limit <= 90_000:
+        primes, index, endPoint, result = [False, True] * (limit//2+1), 3, ceil(limit**0.5) + 1, [2]
+        while index <= endPoint: #sqrt of limit is the endpoint
+            for compositeNum in range(index ** 2, limit + 1, index * 2): 
+                primes[compositeNum] = False
+            index += 2
+            while not primes[index]:
+                index += 2
+        for x in range(3, len(primes), 2):
+            if primes[x]:
+                result.append(x)
+        return (result)
+    else:
+        primes = list()
+        for x in range(limit):
+            if prime_checker(x):
+                primes.append(x)
+        return primes
+
+def prime_factoriser(n):
+    # I am a rookie hence this implementation. upgrade due, feel free to suggest improvements
+
+    if prime_checker(n):
+        return ([n])
+    prime_factor, list_of_factors = 2, list()
+    while n % prime_factor == 0:
+        n //= prime_factor
+        list_of_factors.append(prime_factor)
+    if n == 1:
+        return (list_of_factors)
+    end_point, prime_factor = ceil(sqrt(n)), 3
+    while prime_factor < end_point+1:
+        if n % prime_factor == 0:
+            n //= prime_factor
+            list_of_factors.append(prime_factor)
+            if n == 1:
+                return(list_of_factors)
+        else:
+            prime_factor += 2
+    list_of_factors.append(n)
+    return(list_of_factors)
+
+
+@execute_this
+def main():
+    print(constructionProblem(200, [1,2,5,10,20,25,50,100,150]))
+
+
